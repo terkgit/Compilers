1874b307135251708df6e02fdc441397bd9d68b8
terkmail <terkmail@gmail.com>
2018-11-15 14:59:57 +0200

    reseting workspace

--- terkel/homework1/bin/run all.bat
+++
@@ -1,8 +0,0 @@
-java homework1 < tree1.txt > out.1
-java homework1 < tree2.txt > out.2
-java homework1 < tree3.txt > out.3
-java homework1 < tree4.txt > out.4
-java homework1 < tree5.txt > out.5
-java homework1 < tree6.txt > out.6
-java homework1 < tree7.txt > out.7
-java homework1 < tree8.txt > out.8
